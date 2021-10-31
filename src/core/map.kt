dea64e8e0cb78834eafa9c3326cd8cb1ae915014
someh <someh2705@gmail.com>
2021-11-01 01:46:03 +0900

    feat: Add curried map before make curry function

--- src/core/map.kt
+++ src/core/map.kt
@@ -10,3 +10,16 @@ fun <T> map(iterable: Iterable<T>, transform: (T) -> T): Iterable<T> {
 
     return new.asIterable()
 }
+
+fun <T> map(transform: (T) -> T): (Iterable<T>) -> Iterable<T> {
+    return { iterable: Iterable<T> ->
+        val iterator = iterable.iterator()
+        val new = arrayListOf<T>()
+
+        while (iterator.hasNext()) {
+            new.add(transform(iterator.next()))
+        }
+
+        new.asIterable()
+    }
+}
