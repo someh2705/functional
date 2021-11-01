19bb76980830d050c0cc580418a7ca67081f3fa9
someh <someh2705@gmail.com>
2021-11-02 00:42:39 +0900

    feat: sequence reduce function

--- src/core/reduce.kt
+++ src/core/reduce.kt
@@ -13,3 +13,17 @@ fun <S, T : S> reduce(iterable: Iterable<T>, operation: (acc: S, T) -> T): T {
 
     return accumulator
 }
+
+fun <S, T : S> reduce(sequence: Sequence<T>, operation: (acc: S, T) -> T): T {
+    val iterator = sequence.iterator()
+    if (!iterator.hasNext()) throw Exception("Empty iterable can't be reduce")
+
+    var accumulator = iterator.next()
+
+    while (iterator.hasNext()) {
+        val element = iterator.next()
+        accumulator = operation(accumulator, element)
+    }
+
+    return accumulator
+}
