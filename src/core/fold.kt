a801ce262a7cea9d7ca26c5817bfe05b80f13bc1
someh <someh2705@gmail.com>
2021-11-02 00:42:21 +0900

    feat: sequence fold function

--- src/core/fold.kt
+++ src/core/fold.kt
@@ -10,3 +10,14 @@ fun <T, R> fold(iterable: Iterable<T>, initial: R, operation: (acc: R, T) -> R):
     }
     return accumulator
 }
+
+fun <T, R> fold(sequence: Sequence<T>, initial: R, operation: (acc: R, T) -> R): R {
+    val iterator = sequence.iterator()
+    var accumulator = initial
+
+    while (iterator.hasNext()) {
+        val element = iterator.next()
+        accumulator = operation(accumulator, element)
+    }
+    return accumulator
+}
