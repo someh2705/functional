6d86de38bb264a8f12dcb3ed1a9a0ae45acadc10
someh <someh2705@gmail.com>
2021-11-02 00:43:08 +0900

    feat: lazy take function

--- src/core/take.kt
+++ src/core/take.kt
@@ -25,3 +25,42 @@ fun <T> take(n: Int): (Iterable<T>) -> Iterable<T> {
         take(iterable, n)
     }
 }
+
+class TakeSequence<T>
+constructor(
+    private val sequence: Sequence<T>,
+    private val count: Int
+) : Sequence<T> {
+    init {
+        require(count >= 0) { "count must be non-negative, but was $count" }
+    }
+
+    override fun iterator(): Iterator<T> = object : Iterator<T> {
+        var left = count
+        var iterator = sequence.iterator()
+
+        override fun next(): T {
+            if (left == 0) {
+                throw NoSuchElementException()
+            }
+            left--
+            return iterator.next()
+        }
+
+        override fun hasNext(): Boolean {
+            return left > 0 && iterator.hasNext()
+        }
+    }
+
+}
+
+fun <T> takeL(sequence: Sequence<T>, n: Int): Sequence<T> {
+    require(n >= 0) { "Requested element count $n is less than zero." }
+    return TakeSequence(sequence, n)
+}
+
+fun <T> takeL(n: Int): (Sequence<T>) -> Sequence<T> {
+    return { sequence: Sequence<T> ->
+        takeL(sequence, n)
+    }
+}
