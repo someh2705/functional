a3d0b32223e68eb29611560f44899416d5655ffe
someh <someh2705@gmail.com>
2021-11-02 00:42:59 +0900

    sequence pipe function

--- src/core/pipe.kt
+++ src/core/pipe.kt
@@ -1,5 +1,6 @@
 package core
 
+@JvmName("iterable pipe")
 fun <T> pipe(
     vararg functions: (Iterable<T>) -> Iterable<T>,
     last: ((Iterable<T>) -> Any)? = null
@@ -7,6 +8,25 @@ fun <T> pipe(
     return { iterable: Iterable<T> ->
         go(
             iterable,
+            *functions
+        ) {
+            if (last != null) {
+                last(it)
+            } else {
+                it
+            }
+        }
+    }
+}
+
+@JvmName("sequence pipe")
+fun <T> pipe(
+    vararg functions: (Sequence<T>) -> Sequence<T>,
+    last: ((Sequence<T>) -> Any)? = null
+): (Sequence<T>) -> Any {
+    return { sequence: Sequence<T> ->
+        go(
+            sequence,
             *functions
         ) {
             if (last != null) {
